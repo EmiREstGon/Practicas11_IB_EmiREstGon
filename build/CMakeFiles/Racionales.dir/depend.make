32ffa5a123e412d2c1307a6051bd90f64900858d
usuario <emiestgon@gmail.com>
2021-01-05 17:28:08 +0000

    build

---
+++ build/CMakeFiles/Racionales.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Racionales.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/complejo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/complejo.dir/2.Complejos/complejo.cc.o"
+  "CMakeFiles/complejo.dir/2.Complejos/complejos.cc.o"
+  "../bin/complejo.pdb"
+  "../bin/complejo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/complejo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
