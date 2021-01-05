32b9e172b6a7c4654e336c5b1d2fac0fd17ad2e5
usuario <emiestgon@gmail.com>
2021-01-05 17:54:33 +0000

    changes

--- 1.Fechas/fechas.cc
+++ 1.Fechas/fechas.cc
@@ -1,3 +1,13 @@
+/** 
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author Emi Est Gon
+ * @date 5.jan.2021
+ *
+ */
 #include "fechas.h"
 #include "fecha.h"
 
